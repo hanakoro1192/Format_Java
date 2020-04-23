544c8bbecc8562258cc8c35647460aad43d7d8aa
宇都宮靖之 <csya18003@g.nihon-u.ac.jp>
2020-04-23 05:33:55 -0700

    KuKu

---
+++ KuKu/KuKu.java
@@ -0,0 +1,15 @@
+package KuKu;
+
+public class KuKu {
+    public static void main(String[] args){
+        // System.out.println(args[0]);
+        int kuku;
+
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                kuku = i * j;
+            }
+            System.out.println("KuKuを表示");
+        }
+    }
+}
