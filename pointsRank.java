fdd132d742f0c1589f4d7fa3e5c3a5c409919bfb
宇都宮靖之 <csya18003@g.nihon-u.ac.jp>
2020-05-07 04:18:18 -0700

    pointRankの導入

---
+++ pointsRank.java
@@ -0,0 +1,19 @@
+import  java.io.*;
+
+public  class pointsRank{
+    public static void main(String[] args) throws IOException{
+        int i;
+        int count = 0;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        int x = 0;
+
+        BufferedReader be = new BufferedReader(new InputStreamReader(System.in));
+        for(i = 0; i < count; i++){
+            System.out.println();
+        }
+
+    }
+}
