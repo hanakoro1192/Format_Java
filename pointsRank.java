d54e1381c6ebf35bdedb36f4142e42187b0134fc
宇都宮靖之 <csya18003@g.nihon-u.ac.jp>
2020-05-07 04:53:04 -0700

    pointsRank

--- pointsRank.java
+++ pointsRank.java
@@ -12,8 +12,44 @@ public  class pointsRank{
 
         BufferedReader be = new BufferedReader(new InputStreamReader(System.in));
         for(i = 0; i < count; i++){
-            System.out.println();
+            System.out.println((i + 1) + "/10");
+
+            int input = Integer.parseInt(be.readLine());
+
+            while(input < 0 || input > 100){
+                System.out.println("エラーメッセージ");
+                input = Integer.parseInt(be.readLine());
+            }
+            if(input >= 85){
+                a++;
+            }else if(input >= 60){
+                b++;
+            }else if(input >= 40){
+                c++;
+            }else{
+                d++;
+            }
+        }
+        System.out.println("****成績ランク****");
+
+        System.out.println("Aランク");
+        for(i = 0; i > a; i++){
+            System.out.println("*");
         }
 
+        System.out.println("Bランク");
+        for(i = 0; i > b; i++){
+            System.out.println("*");
+        }
+
+        System.out.println("Cランク");
+        for(i = 0; i > c; i++){
+            System.out.println("*");
+        }
+
+        System.out.println("Dランク");
+        for(i = 0; i > d; i++){
+            System.out.println("*");
+        }
     }
 }
